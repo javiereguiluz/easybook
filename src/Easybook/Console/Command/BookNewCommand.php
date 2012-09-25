31eb666aaf3f869f1421eed8478c55fe6ca1dedb
Hugo Hamon <hugo.hamon@sensio.com>
2012-09-24 18:49:15 -0700

    Refactored the book:new command by introducing a new BookGenerator object.

--- src/Easybook/Console/Command/BookNewCommand.php
+++ src/Easybook/Console/Command/BookNewCommand.php
@@ -18,9 +18,10 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Easybook\DependencyInjection\Application;
-use Easybook\Util\Validator;
 use Easybook\Events\EasybookEvents as Events;
 use Easybook\Events\BaseEvent;
+use Easybook\Generator\BookGenerator;
+use Easybook\Util\Validator;
 
 class BookNewCommand extends BaseCommand
 {
@@ -52,46 +53,29 @@ class BookNewCommand extends BaseCommand
 
         // TODO: extensibility: user should be allowed to define the slug
         $slug = $this->app->get('slugger')->slugify($title);
-        $bookDir = $dir.'/'.$slug;
 
         $this->registerPlugins();
         $this->app->dispatch(Events::PRE_NEW, new BaseEvent($this->app));
 
-        // check if `$bookDir` directory is available
-        // if not, create a unique directory name appending a numeric suffix
-        $i = 1;
-        while (file_exists($bookDir)) {
-            $bookDir = $dir.'/'.$slug.'-'.$i++;
-        }
-
-        // create the skeleton of the new book
-        // don't use mirror() method because git deletes empty directories
-        $skeletonDir = $this->app['app.dir.skeletons'].'/Book';
-        $this->app->get('filesystem')->mkdir($bookDir.'/Contents');
-        $this->app->get('filesystem')->copy(
-            $skeletonDir.'/Contents/chapter1.md',
-            $bookDir.'/Contents/chapter1.md'
-        );
-        $this->app->get('filesystem')->copy(
-            $skeletonDir.'/Contents/chapter2.md',
-            $bookDir.'/Contents/chapter2.md'
-        );
-        $this->app->get('filesystem')->mkdir($bookDir.'/Contents/images');
-        $this->app->get('filesystem')->mkdir($bookDir.'/Output');
-        $this->app->renderFile($skeletonDir, 'config.yml.twig', $bookDir.'/config.yml', array(
+        $generator = new BookGenerator();
+        $generator->setFilesystem($this->app->get('filesystem'));
+        $generator->setSkeletonDirectory($this->app['app.dir.skeletons'].'/Book');
+        $generator->setBookDirectory($dir.'/'.$slug);
+        $generator->setConfiguration(array(
             'generator' => array(
                 'name'    => $this->app['app.name'],
                 'version' => $this->app['app.version']
             ),
             'title' => $title,
         ));
+        $generator->generate();
 
         $this->app->dispatch(Events::POST_NEW, new BaseEvent($this->app));
 
         $output->writeln(array(
             '',
             ' <bg=green;fg=black> OK </> You can start writing your book in the following directory:',
-            ' <comment>'.realpath($bookDir).'</comment>',
+            ' <comment>'.realpath($generator->getBookDirectory()).'</comment>',
             ''
         ));
     }
