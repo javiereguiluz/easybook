e4771fe4d50c8bbdca9d48854dc06b2097165c02
Javier Eguiluz <javier.eguiluz@gmail.com>
2013-07-01 18:55:14 +0200

    Replaced "exit" functions by proper exceptions in the script used to build easybook as a ZIP file.
    This error was found by SensioLabs Insight (analysis #1):
    
      exit() and die() functions should be avoided

--- app/Bin/build.php
+++ app/Bin/build.php
@@ -15,9 +15,10 @@ use Easybook\Util\Compressor;
 
 // check that this command is executed from the root directory
 if ('app/Bin/build.php' != $argv[0]) {
-    echo "[ERROR] This command can only be executed from the root directory as follows:\n\n"
-         ."$ php app/Bin/build.php\n";
-    exit;
+    throw new \RuntimeException(sprintf(
+        "\n[ERROR] This command can only be executed from the root directory as follows:\n\n%s\n\n",
+        "$ php app/Bin/build.php"
+    ));
 }
 
 echo "\nBuilding easybook ZIP file\n".str_repeat('=', 80)."\n";
@@ -28,9 +29,10 @@ $output = array();
 $status = null;
 exec("composer update", $output, $status);
 if (0 !== $status) {
-    echo "\n [ERROR] There was an error updating vendors:\n";
-    echo implode("\n", $output)."\n\n";
-    exit;
+    throw new \RuntimeException(sprintf(
+        "\n[ERROR] There was an error updating vendors:\n%s\n\n",
+        implode("\n", $output)
+    ));
 }
 
 // execute tests
@@ -39,9 +41,10 @@ $output = array();
 $status = null;
 exec("phpunit", $output, $status);
 if (0 !== $status) {
-    echo "\n [ERROR] There was an error executing tests:\n";
-    echo implode("\n", $output)."\n\n";
-    exit;
+    throw new \RuntimeException(sprintf(
+        "\n[ERROR] There was an error executing tests:\n%s\n\n",
+        implode("\n", $output)
+    ));
 }
 
 // build ZIP file
